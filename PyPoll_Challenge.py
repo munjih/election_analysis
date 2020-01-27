de7fcb1ff0eb45861a94f93fae0721d758d86a5d
munjih <59781578+munjih@users.noreply.github.com>
2020-01-27 13:01:37 -0800

    Adding the election analysis

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -1,10 +1,3 @@
-# Retrieve data
-# 1. Total number of votes cast
-# 2. Complet list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote
-
 import csv
 import os
 
